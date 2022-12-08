bdeed8e2a7488ebe2455353846a29e5ef35d43b3
davelevine <dave@davelevine.io>
2022-12-08 11:09:32 -0500

    Updated CSP headers

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const handle = async (request) => {
     headers: {
       "Content-Type": "text/plain; charset=utf-8",
       "Cache-Control": "no-cache, no-store, must-revalidate",
-      "Content-Security-Policy": "default-src * data: 'unsafe-eval' 'unsafe-inline' always",
+      "Content-Security-Policy" : "upgrade-insecure-requests",
+        "Strict-Transport-Security" : "max-age=1000",
+        "X-Xss-Protection" : "1; mode=block",
+        "X-Frame-Options" : "DENY",
+        "X-Content-Type-Options" : "nosniff",
+        "Referrer-Policy" : "strict-origin-when-cross-origin",
       "X-Did-You-Know": "You can use \"curl -4\" or \"curl -6\" to get either IP address!",
       "X-View-Source": "https://github.com/davelevine/checkip",
     },
